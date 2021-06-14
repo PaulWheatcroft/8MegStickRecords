faca95c539af0595110e68222fa2680d02a77638
PaulWheatcroft <paul_wheatcroft@hotmail.com>
2021-06-14 21:58:44 +0000

    "Dev: Begin the transition to data drive. Moving the releases to a JSON array. releases.js built to inport JSON file and built template for displaying on the index.html page

---
+++ javascript/releases.js
@@ -0,0 +1,48 @@
+$.getJSON('data/releases.json', function(data){
+    $(data).each(function(i) {
+        releaseHTML = `
+        <div class="release-container p-2 wow animate__animated animate__fadeIn">
+            <div class="release-cover" id="${data[i].id}">
+            </div>
+            <div class="release-details">
+                <h2>${data[i].title}</small></h2>
+                <h3>${data[i].artist}</h3>
+                <ul class="list-inline">
+                    <li class="list-inline-item">
+                        <a href="${data[i].spotify}" title="Play ${data[i].title} on Spotify" target="_blank" class="release-icon">
+                            <img src="images/spotify.svg" alt="Spotify">
+                        </a>
+                    </li>
+                    <li class="list-inline-item">
+                        <a href="${data[i].apple_music}" title="Play ${data[i].title} on Apple Music" target="_blank" class="release-icon">
+                            <img src="images/applemusic.svg" alt="Apple Music">
+                        </a>
+                    </li>
+                    <li class="list-inline-item">
+                        <a href="${data[i].youtube_music}" title="Play ${data[i].title} on YouTube Music" target="_blank" class="release-icon">
+                            <img src="images/youtubemusic.svg" alt="YouTube Music">
+                        </a>
+                    </li>
+                    <li class="list-inline-item">
+                        <a href="${data[i].deezer}" title="Play ${data[i].title} on Deezer" target="_blank" class="release-icon">
+                            <img src="images/deezer.svg" alt="Deezer">
+                        </a>
+                    </li>
+                    <li class="list-inline-item">
+                        <a href="${data[i].titdal}" title="Play ${data[i].title} on Tidal" target="_blank" class="release-icon">
+                            <img src="images/tidal.svg" alt="Tidal">
+                        </a>
+                    </li>
+                    <li class="list-inline-item">
+                        <a href="${data[i].amazon_music}" title="Play ${data[i].title} on Amazon Music" target="_blank" class="release-icon">
+                            <img src="images/amazon.svg" alt="Amazon Music">
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        `;
+        console.log(releaseHTML);
+    })    
+})
+
