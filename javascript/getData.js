04749cb39f8beac2bef3c3e77987b8f913afc5d5
Paul Wheatcroft <paul.wheatcroft@LEGL-F4W69TMV9F.local>
2024-09-20 09:50:28 +0100

    added ability to get json data

---
+++ javascript/getData.js
@@ -0,0 +1,24 @@
+async function getData() {
+    const url = "./data/releases.json";
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+
+        const json = await response.json();
+        console.log(json);
+        const { createApp, ref } = Vue
+        createApp({
+            setup() {
+                const releases = json
+                return {
+                    releases
+                }
+            }
+        }).mount('#app')
+    } catch (error) {
+        console.error(error.message);
+    }
+}
+getData()
